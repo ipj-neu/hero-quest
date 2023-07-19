bf192e0fe81a1a90aabdea93ea66fbd80cd84f8b
johnsonisaacpeter <ijohnson@student.neumont.edu>
2023-07-18 20:57:09 -0600

    responses in create-session. other small changes

--- lambdas/wss/create-session.py
+++ lambdas/wss/create-session.py
@@ -1,20 +1,26 @@
 import string, secrets, logging
 from botocore.exceptions import ClientError
 from boto3 import resource
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from util import log_error, post_to_connection
 
 def handler(event, context):
-    con_id = event.get("requestContext", {}).get("connectionId")
     try:
+        cxt = event.get("requestContext", {})
+        con_id = cxt.get("connectionId")
+        domain = cxt.get("domainName")
         code_vals = string.ascii_uppercase + string.digits
+        code = "".join(secrets.choice(code_vals) for _ in range(5))
         session = {
-            "sessionCode": "".join(secrets.choice(code_vals) for _ in range(5)),
+            "sessionCode": code,
             "dm": con_id,
         }
-        resource("dynamodb").Table("sessions-v1").put_item(Item=session)
+        try:
+            resource("dynamodb").Table("sessions-v1").put_item(Item=session)
+        except ClientError as e:
+            log_error(e) 
+            return {"statusCode": 500}
+        post_to_connection({"sessionCode": code}, con_id, code, domain)
         return {"statusCode": 201}
-    except ClientError as e:
-        logger.exception(getattr(e, "message", repr(e)))
-        return 500
+    except Exception as e:
+        log_error(e)
+        return {"statusCode": 500}
--- lambdas/wss/update-dm.py
+++ lambdas/wss/update-dm.py
@@ -1,14 +1,18 @@
-from util import post_to_connection, get_session
+from util import post_to_connection, get_session, log_error
 from boto3 import resource
 from json import loads
 
 def handler(event, context):
-    body = loads(event.get("body", {}))
-    session_tbl = resource("dynamodb").Table("sessions-v1")
-    session_code = body.get("sessionCode", None)
+    try:
+        body = loads(event.get("body", {}))
+        session_tbl = resource("dynamodb").Table("sessions-v1")
+        session_code = body.get("sessionCode", None)
 
-    valid, session = get_session(session_code, session_tbl)
-    hero = body.get("hero", None)
-    if not valid or hero == None: return {"statusCode": 404}
-    post_to_connection(hero, session.get("dm"), session_code, event.get("requestContext", {}).get("domainName", ""), session_tbl)
-    return {"statusCode": 200}
+        valid, session = get_session(session_code, session_tbl)
+        hero = body.get("hero", None)
+        if not valid or hero == None: return {"statusCode": 404}
+        post_to_connection(hero, session.get("dm"), session_code, event.get("requestContext", {}).get("domainName", ""), session_tbl)
+        return {"statusCode": 200}
+    except Exception as e:
+        log_error(e)
+        return {"statusCode": 500}
